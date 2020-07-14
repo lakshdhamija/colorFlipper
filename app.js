ef5368a694e4f8087344a0e9ea97897b34ae327e
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-07-15 01:56:11 +0530

    add Project

---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+    const randNum = getRandomNumber();
+    document.body.style.backgroundColor = colors[randNum];
+    color.textContent = colors[randNum];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
